f83cb6c6db70744183cda0ffb2bf1b695198ba96
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-09-30 08:21:52 +0300

    [FIX] safe migration for product_harmonized_system
    Signed-off-by: Ahmet Yigit Budak <yigit@altinkaya.com>

--- product_harmonized_system/migrations/16.0.2.0.0/pre-migration.py
+++ product_harmonized_system/migrations/16.0.2.0.0/pre-migration.py
@@ -4,6 +4,9 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_columns(env.cr, {"hs_code": [("company_id", None)]})
-    rule = env.ref("product_harmonized_system.hs_code_company_rule")
-    if rule:
-        rule.unlink()
+    try:
+        rule = env.ref("product_harmonized_system.hs_code_company_rule")
+        if rule:
+            rule.unlink()
+    except Exception:
+        pass
