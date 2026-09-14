f30353d3160b51b2b621e3d04a282f6beee50009
Burak Kaan Alkan <bkaanalkan@gmail.com>
2026-09-14 14:25:38 +0300

    Fix failures exposed by clean CI baseline checks

--- product_harmonized_system/migrations/16.0.2.0.0/pre-migration.py
+++ product_harmonized_system/migrations/16.0.2.0.0/pre-migration.py
@@ -4,9 +4,8 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_columns(env.cr, {"hs_code": [("company_id", None)]})
-    try:
-        rule = env.ref("product_harmonized_system.hs_code_company_rule")
-        if rule:
-            rule.unlink()
-    except Exception:
-        pass
+    rule = env.ref(
+        "product_harmonized_system.hs_code_company_rule", raise_if_not_found=False
+    )
+    if rule:
+        rule.unlink()
